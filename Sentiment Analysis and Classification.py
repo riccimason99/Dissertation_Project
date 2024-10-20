2eb57b97d68b9c1629a31e1e76730f20107abcc1
Mason Ricci <144854189+riccimason99@users.noreply.github.com>
2024-10-19 22:46:06 -0400

    Update Sentiment Analysis and Classification.py

--- Sentiment Analysis and Classification.py
+++ Sentiment Analysis and Classification.py
@@ -10,12 +10,12 @@ import pandas as pd
 import numpy as np
 import math
 import nltk
-nltk.download('vader-lexicon')
+nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import statsmodel.api as sm
+import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score f1_score
+from sklearn.metrics import accuracy_score, f1_score
 import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score, KFold
 from sklearn.metrics import confusion_matrix
@@ -30,7 +30,7 @@ import seaborn as sns
 
 
 # Import data
-all_text = pd.read_csv('the data.csv')
+all_text = pd.read_csv('/Users/riccimason99/Downloads/Dissertation_2024/all_text_data_frame_clean.csv')
 
 ############
 ########################
@@ -39,7 +39,7 @@ all_text = pd.read_csv('the data.csv')
 ############
 
 # first untokenize text so it can be used easily for sentiment analysis
-all_text['untokenized'] = all_text['tokens'].apply(lambda tokens: ''join(tokens).replace("', '", ' ').replace("['", '').replace("']", ''))
+all_text['untokenized'] = all_text['tokens'].apply(lambda tokens: ''.join(tokens).replace("', '", ' ').replace("['", '').replace("']", ''))
 
 
 # Set sentminet analizer 
@@ -52,9 +52,9 @@ pos = []
 neu = []
 # apply analizer to each piece of text and get compund value
 for text in all_text['untokenized']:
-    score = analyzer.polarity.scores(text)
+    score = analyzer.polarity_scores(text)
     compounds.append(score['compound'])
-    neg.append(score[' neg'])
+    neg.append(score['neg'])
     pos.append(score['pos'])
     neu.append(score['neu'])
 
@@ -86,7 +86,7 @@ results = model.fit()
 
 
 # New colum w predicted probability 
-all_text['pred_prob'] = results.predict(all_text[['const','neg']]])
+all_text['pred_prob'] = results.predict(all_text[['const','neg']])
 
 # sort by X variable 
 all_text = all_text.sort_values('neg')
@@ -101,12 +101,12 @@ all_text = all_text.sort_values('neg')
 ########################
 ############
 
-X = all_text[['compounds', 'neg', 'pos', 'nu']]
+X = all_text[['compounds', 'neg', 'pos', 'neu']]
 y = all_text['binary']
-X_train,X_test, y_train, y_test = train_test_splitt(X, y, test_size = .2)
+X_train,X_test, y_train, y_test = train_test_split(X, y, test_size = .2)
 
 #clf = LogisticRegression()                 
-#clf = LinearDiscriminantAnalyis()         
+#clf = LinearDiscriminantAnalysis()         
 #clf = KNeighborsClassifier(n_neighbors=3) 
 #clf = SVC(kernel='rbf')                  
 clf = GaussianNB()                       
@@ -114,8 +114,8 @@ clf = GaussianNB()
 
 # Use cross validation to select model and parameters  
 kfold = KFold(n_splits = 5, shuffle = True)
-scores = cross_val_score(clf X_train, y_train, cv = kfold, scoring = 'accuracy')
-print(f'Cross-validated Accuracy: {scores.mean():.2} (+/- {scores.std():.2f})')
+scores = cross_val_score(clf, X_train, y_train, cv = kfold, scoring = 'accuracy')
+print(f'Cross-validated Accuracy: {scores.mean():.2f} (+/- {scores.std():.2f})')
 
 # Fit model to all training data and test on test set
 clf.fit(X_train, y_train)
@@ -128,9 +128,11 @@ print(accuracy_score(y_test, y_pred))
 # Generate the confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Display the confusion matrx
+# Display the confusion matrix
 disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot(cmap=pltcm.Blues)
+disp.plot(cmap=plt.cm.Blues)
 # Show the plot
 plt.show()
+
+
 
